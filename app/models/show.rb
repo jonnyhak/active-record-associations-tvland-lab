4d79718933c4a784327921eb99017b03e1bbf1c5
jonnyhak <jonnyhak@gmail.com>
2020-10-15 19:07:56 -0400

    chain building needed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :newtwork
+    has_many :actors, through: :characters
+    
+
+    def actors_list
+        characters.map  do |character|
+           "#{character.actor.first_name} #{character.actor.last_name}" 
+        end
+    end
+
   
 end
